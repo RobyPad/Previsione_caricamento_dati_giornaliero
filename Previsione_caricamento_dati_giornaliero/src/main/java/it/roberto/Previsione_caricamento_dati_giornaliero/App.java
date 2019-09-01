fe92e1ed147dd320de50a284d2870a13ce1a0c85
Utente <Utente@DESKTOP-TTN64SK>
2019-09-01 14:58:04 +0200

    FIRST COMMIT

---
+++ Previsione_caricamento_dati_giornaliero/src/main/java/it/roberto/Previsione_caricamento_dati_giornaliero/App.java
@@ -0,0 +1,13 @@
+package it.roberto.Previsione_caricamento_dati_giornaliero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
